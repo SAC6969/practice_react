bf6c8cac974a3cc237ed33936e6c595f39c53431
Sachin Varma <sachinvermaa1234@gmail.com>
2022-05-20 18:23:50 +0530

    batching

--- Test.js
+++ Test.js
@@ -11,18 +11,29 @@ class Test extends Component {
 
     handleAdd = ()=> {
         //type one of state
-        // this.setState({
-        //     add : this.state.add +1
-        // })
+
+        // when this.setState called more then one but render one time beacuse of batching
+        this.setState({
+            add : this.state.add +1
+        })
+
+        this.setState({
+            add : this.state.add +1
+        })
+
+        this.setState({
+            add : this.state.add +1
+        })
 
         //type two of state
-        this.setState((prevState)=>{
-            return{
-                add : prevState.add + 1
-            }
-        })
+        // this.setState((prevState)=>{
+        //     return{
+        //         add : prevState.add + 1
+        //     }
+        // })
     }
   render() {
+    console.log('render')
     return (
       <div>
           <div>{this.state.name}</div>
